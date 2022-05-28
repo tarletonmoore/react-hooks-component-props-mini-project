83d83bb97a8abd7cd484baa672d24e9abc2ff700
John Tarleton Moore <tarletonmoore@gmail.com>
2022-05-28 13:30:37 -0400

    commit tree e199061abacbc2d325c069626f4e24c35aff052a

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import blogData from "../data/blog"
+
+function Header({ name }) {
+
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    )
+}
+
+export default Header
